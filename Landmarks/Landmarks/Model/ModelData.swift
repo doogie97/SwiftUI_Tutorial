de259aa3ecd0eed350557ba581e91feb34e99eb7
Doogie <chris_97@naver.com>
2024-05-11 12:15:13 +0900

    feat: Json파일 불러오는 기능 구현

---
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -0,0 +1,31 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Doogie on 5/11/24.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ fileName: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: fileName, withExtension: nil) else {
+        fatalError("파일 읽어올 수 없음")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("데이터 변환 실패")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("디코딩 실패")
+    }
+}
