da64a1d4c68356e27b7b1c3ad913ace2c0d12c48
Doogie <chris_97@naver.com>
2024-05-17 21:21:01 +0900

    refactor: HikeBadgeView Vstack에 포함

--- Landmarks/Landmarks/View/Hikes/HikeBadgeView.swift
+++ Landmarks/Landmarks/View/Hikes/HikeBadgeView.swift
@@ -11,13 +11,15 @@ struct HikeBadgeView: View {
     var name: String
     
     var body: some View {
-        Badge()
-            .frame(width: 300, height: 300)
-            .scaleEffect(1.0 / 3.0)
-            .frame(width: 100, height: 100)
-        Text(name)
-            .font(.caption)
-            .accessibilityLabel("Badge for \(name)")
+        VStack(alignment: .center) {
+            Badge()
+                .frame(width: 300, height: 300)
+                .scaleEffect(1.0 / 3.0)
+                .frame(width: 100, height: 100)
+            Text(name)
+                .font(.caption)
+                .accessibilityLabel("Badge for \(name)")
+        }
     }
 }
 
