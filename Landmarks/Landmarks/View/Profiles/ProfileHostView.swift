4b122b71d060db04668f80a365a2f5fc33406e6a
Doogie <chris_97@naver.com>
2024-05-17 21:34:56 +0900

    feat: ProfileHostView에 editMode프로퍼티 생성, 그에 따른 뷰 반환

--- Landmarks/Landmarks/View/Profiles/ProfileHostView.swift
+++ Landmarks/Landmarks/View/Profiles/ProfileHostView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ProfileHostView: View {
+    @Environment(\.editMode) var editMode
     @Environment(ModelData.self) var modelData
     @State private var draftProfile = Profile.default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                Spacer()
+                EditButton()
+            }
+            if editMode?.wrappedValue == .inactive {
                 ProfileSummaryView(profile: modelData.profile)
+            } else {
+                Text("Profile Editor")
+            }
+            
         }
         .padding()
     }
